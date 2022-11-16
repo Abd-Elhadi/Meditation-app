e423efd2e1115874308cbda293f0559242843f5f
Abdelhadi Omar <abdelhadioumar@gmail.com>
2022-11-16 17:14:28 +0200

    Add animation

---
+++ app.js
@@ -0,0 +1,51 @@
+const app = () => {
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+    const sounds = document.querySelectorAll('.sound-picker button');
+    const tmieDisplay = document.querySelector('.time-display');
+    const outlineLength = outline.getTotalLength();
+    let fakeDuration = 60;
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+    // play sound
+    play.addEventListener('click', () => {
+        checkPlaying(song);
+    });
+
+    // Create a function specific to stop and play the sounds
+    const checkPlaying = song => {
+        if (song.paused) {
+            song.play();
+            video.play();
+            play.src = './svg/pause.svg';
+        } else {
+            song.pause();
+            video.pause();
+            play.src = './svg/play.svg';
+        }
+    }
+
+    // We can animate the cirlce
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60);
+
+        // Animate the circle
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+
+        // Animate the text
+        tmieDisplay.textContent = `${minutes}:${seconds}`
+    };
+
+
+};
+
+
+app();
